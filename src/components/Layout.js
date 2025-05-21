17bc7edd8e48123feda462b0278b35269a249698
dschwenk94 <schwenkedavis@gmail.com>
2025-05-20 22:29:37 -0600

    Update Layout component to include Footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from './Header';
+import Footer from './Footer';
 
-const Main = styled.main`
-  min-height: calc(100vh - 100px);
-  width: 100%;
-  position: relative;
-  z-index: 1;
+const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 `;
 
-// We're not using a separate Footer component since it's integrated into the HomePage
+const MainContent = styled.div`
+  flex: 1;
+`;
+
 const Layout = ({ children }) => {
   return (
-    <>
+    <LayoutContainer>
       <Header />
-      <Main>{children}</Main>
-    </>
+      <MainContent>{children}</MainContent>
+      <Footer />
+    </LayoutContainer>
   );
 };
 
